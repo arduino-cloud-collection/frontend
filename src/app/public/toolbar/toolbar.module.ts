0dd88049e11f360cb04bd434e699ff47094e3816
Yannis Storrer <yannis.storrer@web.de>
2020-10-20 20:47:59 +0200

    Added basic toolbar

--- src/app/public/toolbar/toolbar.module.ts
+++ src/app/public/toolbar/toolbar.module.ts
@@ -1,13 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ToolbarComponent } from './toolbar/toolbar.component';
-
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {FlexLayoutModule} from '@angular/flex-layout';
 
 
 @NgModule({
   declarations: [ToolbarComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    MatToolbarModule,
+    MatIconModule,
+    FlexLayoutModule
+
   ],
   exports: [
     ToolbarComponent
